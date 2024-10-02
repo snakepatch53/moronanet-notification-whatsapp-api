14929bea3b11e57fd891a262ef380cf4f083da96
snakepatch53 <snakepatch53@gmail.com>
2024-10-02 01:33:12 -0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   .gitignore
    	modified:   .wwebjs_cache/2.3000.1016981433.html
    	modified:   src/app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ dotenv.config();
             host: process.env.DB_HOST,
             port: parseInt(process.env.DB_PORT),
             username: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
+            password: process.env.DB_PASS,
             database: process.env.DB_NAME,
             entities: [join(__dirname, '**', '*.entity.{ts,js}')],
             synchronize: process.env.PROD === 'true' ? false : true,
