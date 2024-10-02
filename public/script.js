82e72bf54ea6af1e36a4e9351265617e2306734e
snakepatch53 <snakepatch53@gmail.com>
2024-10-02 03:24:21 -0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	deleted:    .wwebjs_cache/2.3000.1016979389.html
    	deleted:    .wwebjs_cache/2.3000.1016981433.html
    	modified:   public/script.js
    	modified:   views/qr.hbs

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 const socket = io(); // Conexión al servidor de WebSocket
 const qrCodeContainer = document.getElementById('qrCodeContainer');
+const logsContainer = document.getElementById('logs-container');
 
 if (!qrCodeLoaded) {
     qrCodeContainer.innerHTML = `<p>Sessión Iniciada ✅</p>`;
@@ -22,5 +23,17 @@ socket.on('qrCode', (qr) => {
     `;
 });
 socket.on('log-response', (data) => {
-    console.log(data);
+    const content =
+        `
+        <div id="logs-container" class="flex items-start space-x-4 shadow rounded-lg p-5 bg-gray-50 border">
+            <div class="relative">
+                <img src="/message-icon.png" class="flex h-8 aspect-square object-contain p-1.5 items-center justify-center rounded-full bg-gray-100"></img>
+            </div>
+            <div class="flex-1">
+                <p class="text-sm font-medium">${data.detail}</p>
+                <p class="text-xs text-gray-500">${data.dateTime}</p>
+            </div>
+        </div>
+    ` + logsContainer.innerHTML;
+    logsContainer.innerHTML = content;
 });
